9194a4f995c1939f21298c08bee6bd6f06422f3a
MomenMorgan <mominmorgan10@gmail.com>
2023-10-30 18:05:38 -0700

    change doc

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""this module handles the statues code route"""
+"""This module handles the statues code route"""
 from flask import jsonify
 from api.v1.views import app_views
 from models.amenity import Amenity
